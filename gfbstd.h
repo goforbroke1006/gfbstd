cd0ce6af43ccad2138ea16d28dc24f96e1dea080
goforbroke <go.for.broke1006@gmail.com>
2020-10-18 18:56:55 +0300

    Initial commit

---
+++ gfbstd.h
@@ -0,0 +1,84 @@
+//
+// Created by GOFORBROKE on 18.10.2020.
+//
+
+#ifndef GFBSTD_H
+#define GFBSTD_H
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+namespace gfbstd {
+    namespace strings {
+
+        std::vector<std::string>
+        split(const std::string &s, const std::string &sep) {
+            std::vector<std::string> result;
+
+            if (s.empty()) {
+                return result;
+            }
+
+            if (sep.empty()) {
+                for (char c : s) {
+                    result.push_back(std::string(1, c));
+                }
+                return result;
+            }
+
+            size_t last = 0;
+            size_t position = s.find(sep, last);
+
+            while (position != std::string::npos) {
+                std::string item = s.substr(last, position - last);
+                result.push_back(item);
+
+                last = position + sep.size();
+                position = s.find(sep, last);
+            }
+
+            std::string item = s.substr(last, position - last);
+            result.push_back(item);
+
+            return result;
+        }
+
+        bool
+        isSpace(char c) {
+            return c == ' ' || c == '\n' || c == '\t';
+        }
+
+        std::string
+        trimSpace(const std::string &s) {
+            std::string result = s;
+            while (isSpace(result[0])) {
+                result = result.substr(1);
+            }
+            while (isSpace(result[result.size() - 1])) {
+                result = result.substr(0, result.size() - 1);
+            }
+            return result;
+        }
+
+        std::string
+        replace(const std::string &s, const std::string &_old, const std::string &_new) {
+            std::string result = s;
+
+            size_t last = 0;
+            size_t position = result.find(_old, last);
+
+            while (position != std::string::npos) {
+                result = result.replace(position, _old.length(), _new, 0, _new.length());
+
+                last = position + _new.length();
+                position = result.find(_old, last);
+            }
+
+            return result;
+        }
+
+    }
+}
+
+#endif //GFBSTD_H
---
+++ tests/strings-test.h
@@ -0,0 +1,138 @@
+//
+// Created by GOFORBROKE on 18.10.2020.
+//
+
+#ifndef STRINGS_TEST_H
+#define STRINGS_TEST_H
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "../gfbstd.h"
+
+TEST(strings_split, empty_string) {
+    ASSERT_EQ(
+            gfbstd::strings::split("", ""),
+            std::vector<std::string>()
+    );
+}
+
+TEST(strings_split, empty_separator) {
+    ASSERT_EQ(
+            gfbstd::strings::split("hello", ""),
+            std::vector<std::string>({"h", "e", "l", "l", "o"})
+    );
+}
+
+TEST(strings_split, positive_1) {
+    ASSERT_EQ(
+            gfbstd::strings::split("12,,,23,,,34,,,56", ",,,"),
+            std::vector<std::string>({"12", "23", "34", "56"})
+    );
+}
+
+TEST(strings_split, positive_2) {
+    ASSERT_EQ(
+            gfbstd::strings::split(",,,12,,,23,,,34,,,56", ",,,"),
+            std::vector<std::string>({"", "12", "23", "34", "56"})
+    );
+}
+
+TEST(strings_split, positive_3) {
+    ASSERT_EQ(
+            gfbstd::strings::split("12,,,23,,,34,,,56,,,", ",,,"),
+            std::vector<std::string>({"12", "23", "34", "56", ""})
+    );
+}
+
+TEST(strings_split, positive_4) {
+    ASSERT_EQ(
+            gfbstd::strings::split(",,,12,,,23,,,34,,,56,,,", ",,,"),
+            std::vector<std::string>({"", "12", "23", "34", "56", ""})
+    );
+}
+
+TEST(strings_split, negative_1_separator_not_found) {
+    ASSERT_EQ(
+            gfbstd::strings::split("hello", "world"),
+            std::vector<std::string>({"hello"})
+    );
+}
+
+TEST(strings_split, negative_1_separator_part_in_begin) {
+    ASSERT_EQ(
+            gfbstd::strings::split("hello", "heXX"),
+            std::vector<std::string>({"hello"})
+    );
+}
+
+TEST(strings_split, negative_1_separator_part_in_end) {
+    ASSERT_EQ(
+            gfbstd::strings::split("hello", "XXllo"),
+            std::vector<std::string>({"hello"})
+    );
+}
+
+TEST(strings_split, negative_1_separator_part_in_middle) {
+    ASSERT_EQ(
+            gfbstd::strings::split("hello", "XllX"),
+            std::vector<std::string>({"hello"})
+    );
+}
+
+
+/* trimSpace */
+
+TEST(strings_trimSpace, positive_1_left) {
+    ASSERT_EQ(
+            gfbstd::strings::trimSpace("  hello"),
+            "hello"
+    );
+}
+
+TEST(strings_trimSpace, positive_2_right) {
+    ASSERT_EQ(
+            gfbstd::strings::trimSpace("hello  "),
+            "hello"
+    );
+}
+
+TEST(strings_trimSpace, positive_3_both) {
+    ASSERT_EQ(
+            gfbstd::strings::trimSpace("     hello  "),
+            "hello"
+    );
+}
+
+TEST(strings_trimSpace, positive_3_spec) {
+    ASSERT_EQ(
+            gfbstd::strings::trimSpace("  \n   hello \t\n "),
+            "hello"
+    );
+}
+
+/* replace */
+
+TEST(strings_replace, positive_same_length) {
+    ASSERT_EQ(
+            gfbstd::strings::replace("hello world", "o", "0"),
+            "hell0 w0rld"
+    );
+}
+
+TEST(strings_replace, positive_diff_length_1) {
+    ASSERT_EQ(
+            gfbstd::strings::replace("hello world hello 123 hello", "ell", "++++++"),
+            "h++++++o world h++++++o 123 h++++++o"
+    );
+}
+
+TEST(strings_replace, positive_diff_length_2) {
+    ASSERT_EQ(
+            gfbstd::strings::replace("hello worldhello world", "orl", "_"),
+            "hello w_dhello w_d"
+    );
+}
+
+#endif //STRINGS_TEST_H
